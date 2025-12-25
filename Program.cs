6b61da98fed45d94fe8b7b80152e874783210576
Ye Min Htet <2403803F@student.tp.edu.sg>
2025-12-26 00:49:07 +0800

    Route Access Setup

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<LanguageLearningUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<LanguageLearningContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
